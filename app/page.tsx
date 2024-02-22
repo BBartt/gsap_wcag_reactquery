5c17ccafc38b2990395cd06311706528bd9cf1ba
Bartosz <bartek2@onet.eu>
2024-02-22 23:00:11 +0100

    feat(home page): Remove all logic from app/page.tsx and leave return string statement

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,3 @@
-"use client";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import styles from "./page.module.css";
-
-const queryClient = new QueryClient();
-
-const Test = () => {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["people"],
-    queryFn: () =>
-      fetch("https://swapi.dev/api/people/").then((res) => res.json()),
-  });
-
-  if (isPending) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
-  console.log("data", data);
-
-  return "test";
-};
-
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <QueryClientProvider client={queryClient}>
-        <Test />
-      </QueryClientProvider>
-    </main>
-  );
+export default async function Home() {
+  return "Strona domowa";
 }
