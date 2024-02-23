0093de981a83a9b85328ea6d50c9f75b5faad4f1
Bartosz <bartek2@onet.eu>
2024-02-24 00:35:00 +0100

    feat: Display amount of saved characters in Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,13 @@
+"use client";
 import { RoutesEnum } from "@/constants";
 import Link from "next/link";
 
 import "./styles.scss";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 const Header = () => {
+  const [storedCharacter] = useLocalStorage<ICharacter[]>("characters", []);
+
   return (
     <header className="header">
       <nav className="nav">
@@ -15,7 +19,9 @@ const Header = () => {
             <Link href={RoutesEnum.characters}>Postacie</Link>
           </li>
           <li className="li">
-            <Link href={RoutesEnum.favourites}>Ulubione</Link>
+            <Link href={RoutesEnum.favourites} suppressHydrationWarning>
+              Ulubione ({storedCharacter.length})
+            </Link>
           </li>
         </ul>
       </nav>
